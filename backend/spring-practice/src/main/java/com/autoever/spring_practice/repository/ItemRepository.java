4fbb31ead14d194bb05bd3bbbe309a0cdcb22e19
Gumraze-git <galaxydh4110@gmail.com>
2025-06-09 15:38:20 +0900

    feat(ItemRepository): add query method to find item by description

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/repository/ItemRepository.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.autoever.spring_practice.repository;
 import java.util.List;
 import com.autoever.spring_practice.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 // @Repository: 해당 인터페이스가 데이터 접근 계층이라는 것을 Spring에 알려주는 애너테이션
@@ -19,4 +21,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     // 다음 쿼리 문이 반환된다.
     // SELECT * FROM item WHERE item_name = :itemName OR item_description = :itemDescription;
     List<Item> findByItemNameOrItemDescription(String itemName, String itemDescription);
+
+    // 실제 쿼리 메시지를 만들고 보내는 방법 2가지
+    // @Query: JPQL, nativeQuery
+    // JPQL: OOP 언어로 쿼리를 작성하는 언어이다.
+    @Query("SELECT i FROM Item i WHERE i.itemDescription like %:itemDescription% ORDER BY i.price desc")
+    List<Item> findByItemDescription(@Param("ItemDescription") String itemDescription);
 }
