6f8d6a15cbe562ef97e87e6a61c0fad5beb22de6
Gumraze-git <galaxydh4110@gmail.com>
2025-06-09 12:34:27 +0900

    feat(repository): add spring data jpa query method

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/repository/ItemRepository.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/repository/ItemRepository.java
@@ -1,10 +1,11 @@
 package com.autoever.spring_practice.repository;
 
+import java.util.List;
 import com.autoever.spring_practice.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
+    List<Item> findByItemName(String itemName); // SELECT * FROM item WHERE item_name = ''
 }
