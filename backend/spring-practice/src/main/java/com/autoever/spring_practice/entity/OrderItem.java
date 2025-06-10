c63113db4abf7b7d0a7eefa2f62f4614dbf2113d
Gumraze-git <galaxydh4110@gmail.com>
2025-06-10 14:47:01 +0900

    feat(OrderItem): add OrderItem entity with JPA mappings
    - Create `OrderItem` entity with fields `id`, `item`, `order`, `orderPrice`, `count`, and `regTime`
    - Add relationship mappings:
      - `@ManyToOne` for association with `Item`
      - `@ManyToOne` for association with `Order`
    - Use `@JoinColumn` to define foreign key columns (`item_id`, `order_id`)
    - Include Lombok annotations for getter, setter, and `toString` methods

---
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package com.autoever.spring_practice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+    private LocalDateTime regTime;
+}
