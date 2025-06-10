37abbcdd743cb35072f079cffd2e815cd8c75c3e
Gumraze-git <galaxydh4110@gmail.com>
2025-06-10 10:41:46 +0900

    feat(Cart): add cart entity and define cart id and member id

---
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.autoever.spring_practice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; // cart에 대한 id 생성
+
+    @OneToOne // Member entity와 1대1 매핑
+    @JoinColumn(name = "member_id")
+    private Member member; // 생성된 회원 객체가 와야 한다.
+}
