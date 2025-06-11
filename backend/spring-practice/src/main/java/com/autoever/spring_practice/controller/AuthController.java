e297155daf898cfd6734f3e5a0d3277b28a74ba3
Gumraze-git <galaxydh4110@gmail.com>
2025-06-11 11:21:49 +0900

    feat(AuthController): add API endpoints for user registration, login, and email existence check

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/controller/AuthController.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/controller/AuthController.java
@@ -1,11 +1,38 @@
 package com.autoever.spring_practice.controller;
 
+import com.autoever.spring_practice.dto.LoginReqDto;
+import com.autoever.spring_practice.dto.MemberReqDto;
+import com.autoever.spring_practice.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor // 생성자를 통한 의존성 주입을 받기 위해 생성자를 자동 지정
 @RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    // 회원 가입 여부 확인
+    @GetMapping("/exists/{email}") // 정보가 브라우저 주소창에 보여진다.
+    public ResponseEntity<Boolean> existsByEmail(@PathVariable String email) {
+        boolean isExists = authService.isMember(email);
+        return ResponseEntity.ok(!isExists);
+    }
+
+    // 회원 가입
+    @PostMapping("/sign-up") // body에 정보가 보여진다.
+    public ResponseEntity<Boolean> signUp(@RequestBody MemberReqDto memberReqDto) {
+        boolean isSuccess = authService.signUp(memberReqDto);
+        return ResponseEntity.ok(isSuccess);
+    }
+
+    // 로그인
+    public ResponseEntity<Boolean> login(@RequestBody LoginReqDto loginReqDto) {
+        boolean isSuccess = authService.login(loginReqDto.getEmail(), loginReqDto.getPassword());
+        return ResponseEntity.ok(isSuccess);
+    }
 
 }
