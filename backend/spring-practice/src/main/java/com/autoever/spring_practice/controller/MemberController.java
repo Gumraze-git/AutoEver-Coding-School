e51b8683effa6cd6dcefa0d27c13b0490d98a610
Gumraze-git <galaxydh4110@gmail.com>
2025-06-13 14:08:25 +0900

    feat(MemberController): add MemberController for user management REST API

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/controller/MemberController.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/controller/MemberController.java
@@ -1,25 +1,48 @@
 package com.autoever.spring_practice.controller;
 
+import com.autoever.spring_practice.dto.MemberReqDto;
+import com.autoever.spring_practice.dto.MemberResDto;
+import com.autoever.spring_practice.repository.MemberRepository;
 import com.autoever.spring_practice.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Slf4j
-@RestController
-@RequiredArgsConstructor
+import java.util.List;
+
+@Slf4j // Log 메시지 출력을 위한 어노테이션
+@RestController  // Rest API (GET, POST, PUT, DELETE)
+@RequiredArgsConstructor  // 생성자를 통한 의존성 주입을 받기위해서 생성자를 자동 생성
 @CrossOrigin(origins = {
-        "http://localhost:3000"
-        , "http://localhost:5173"
+        "http://localhost:3000",
+        "http://localhost:5173"
 })
+@RequestMapping("/users")
 public class MemberController {
     private final MemberService memberService;
+
     // 회원 전체 조회
+    @GetMapping("/list")
+    public ResponseEntity<List<MemberResDto>> getMembers() {
+        return ResponseEntity.ok(memberService.findAll());
+    }
 
-    // 회원 상제 조회
+    // 회원 상세 조회
+    @GetMapping("/detail/{email}")
+    public ResponseEntity<MemberResDto> getMember(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.findByEmail(email));
+    }
 
-    // 회원 정보 조회
+    // 회원 정보 수정
+    @PutMapping("/modify")
+    public ResponseEntity<Boolean> updateMember(@RequestBody MemberResDto memberResDto) {
+        return ResponseEntity.ok(memberService.modifyMember(memberResDto));
+    }
 
     // 회원 삭제
+    @DeleteMapping("/delete/{email}")
+    public ResponseEntity<Boolean> deleteMember(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.deleteMember(email));
+    }
 }
