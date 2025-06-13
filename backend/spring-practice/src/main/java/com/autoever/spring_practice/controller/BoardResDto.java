ed7d013b618b3d00e7393445a9732137fdeef090
Gumraze-git <galaxydh4110@gmail.com>
2025-06-13 17:25:59 +0900

    feat(controller): add BoardResDto for board response data

---
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/controller/BoardResDto.java
@@ -0,0 +1,21 @@
+package com.autoever.spring_practice.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardResDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String writerEmail;
+    private String img;
+    private LocalDateTime createTime;
+}
