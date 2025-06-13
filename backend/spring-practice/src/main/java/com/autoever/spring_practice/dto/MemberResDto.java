581a5ad0886db958a4d600edab87db100ea0b04b
Gumraze-git <galaxydh4110@gmail.com>
2025-06-13 15:23:04 +0900

    chore(MemberResDto): remove comments

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberResDto.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberResDto.java
@@ -1,13 +1,5 @@
 package com.autoever.spring_practice.dto;
 
-/* MemberResDto
- * - 회원가입 또는 회원 정보 조회 응답에 사용될 데이터 전송 객체.
- * - email: 사용자의 이메일
- * - name: 사용자의 이름
- * - password: 사용자의 비밀번호
- * - image: 사용자의 이미지
- * - regDate: 사용자 등록 시점
- * */
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
