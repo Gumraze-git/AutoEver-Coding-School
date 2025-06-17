8303dce3e0887f9d7d87772640e31337749e9357
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 10:16:52 +0900

    feat(pracitce_spring_jdbc): initialized spring pratice with jdbc

---
+++ backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/PracticSpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.hd.practice_spring_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticSpringJdbcApplication.class, args);
+	}
+
+}
---
+++ backend/practice_spring_jdbc/src/test/java/com/hd/practice_spring_jdbc/PracticSpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hd.practice_spring_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticSpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
