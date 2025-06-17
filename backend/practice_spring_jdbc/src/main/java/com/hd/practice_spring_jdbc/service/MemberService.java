7f33a99099cb9a18d0d176de3707c8125f50ff25
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 11:19:57 +0900

    feat(MemberService): implement `findAll` and `findMemberByEmail` methods with error handling

--- backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/service/MemberService.java
+++ backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/service/MemberService.java
@@ -1,4 +1,29 @@
 package com.hd.practice_spring_jdbc.service;
 
+import com.hd.practice_spring_jdbc.dao.MemberDao;
+import com.hd.practice_spring_jdbc.dto.MemberDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+
 public class MemberService {
+    private MemberDao memberDao;
+    public List<MemberDto> findAll() {
+        return memberDao.findAllMembers();
+    }
+
+    // 이메일로 회원 조회
+    public Optional<MemberDto> findMemberByEmail(String email) {
+        Optional<MemberDto> result = memberDao.findMemberByEmail(email);
+        if (result.isEmpty()) {
+            throw new RuntimeException("[CUSTOM ERROR MSG] 해당 이메일이 존재하지 않습니다.");
+        }
+        return memberDao.findMemberByEmail(email);
+    }
 }
