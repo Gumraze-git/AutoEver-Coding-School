2177957d6390d7d8e48581cb6e9c7d753c2aa363
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 10:29:43 +0900

    feat(MemberDto): add Lombok annotations for getters and setters

--- backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/dto/MemberDto.java
+++ backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/dto/MemberDto.java
@@ -1,7 +1,12 @@
 package com.hd.practice_spring_jdbc.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@
 public class MemberDto {
     private String name;
     private String email;
