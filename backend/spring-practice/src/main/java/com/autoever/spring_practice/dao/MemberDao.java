51b20d43b92539b2545c1339c3eb7c1296951e3d
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 15:21:45 +0900

    feat(MemberDao): add `MemberRowMapper` for mapping result set to `MemberResDto`

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dao/MemberDao.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dao/MemberDao.java
@@ -1,9 +1,13 @@
 package com.autoever.spring_practice.dao;
 
+import com.autoever.spring_practice.dto.MemberResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,4 +17,17 @@ public class MemberDao {
 
     // 전체 회원 조회
     private static final String SELECT_MEMBER_ALL = "SELECT * FROM member";
+
+    private static class MemberRowMapper implements RowMapper<MemberResDto> {
+        @Override
+        public MemberResDto mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+            return new MemberResDto(
+                    resultSet.getString("email"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getString("image"),
+                    resultSet.getTimestamp("reg_date").toLocalDateTime()
+                    );
+        }
+    }
 }
