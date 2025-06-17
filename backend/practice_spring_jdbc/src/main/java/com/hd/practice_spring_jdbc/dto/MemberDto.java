95b9acd551537ad836f5f341dae5eec64c2987ba
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 10:27:52 +0900

    feat(MemberDto): add MemberDto class to represent member details

---
+++ backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/dto/MemberDto.java
@@ -0,0 +1,10 @@
+package com.hd.practice_spring_jdbc.dto;
+
+import java.time.LocalDateTime;
+
+public class MemberDto {
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime date;
+}
