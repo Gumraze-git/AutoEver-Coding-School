8ddd7cd1cfa14cc7dc278771794cce17182eb4a8
Gumraze-git <galaxydh4110@gmail.com>
2025-06-17 11:42:58 +0900

    feat(MemberController): add `getMemberByEmail` endpoint to fetch member by email

--- backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/controller/MemberController.java
+++ backend/practice_spring_jdbc/src/main/java/com/hd/practice_spring_jdbc/controller/MemberController.java
@@ -5,9 +5,11 @@ import com.hd.practice_spring_jdbc.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
 
 @RestController // JSON 응답을 위한 컨트롤러
 @RequestMapping("/members")
@@ -19,5 +21,13 @@ public class MemberController {
     @GetMapping
     public ResponseEntity<List<MemberDto>> getAllMembers() {
         return ResponseEntity.ok(memberService.findAll());
+    }
+
+    // 이메일로 회원 조회
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberDto> getMemberByEmail(@PathVariable String email) {
+        Optional<MemberDto> result = memberService.findMemberByEmail(email);
+        return result.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
