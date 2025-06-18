1a301624346c89fccdd3a8a11bb05c37c7a1abe9
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 11:37:48 +0900

    feat(JwtFilter): implement `JwtFilter` for token extraction and authentication
    - Add `JwtFilter` to process JWT from Authorization headers.
    - Extract tokens, validate them, and set authentication in the security context.

---
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/jwt/JwtFilter.java
@@ -0,0 +1,41 @@
+package com.autoever.spring_practice.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = resolveToken(request); // 헤더에서 JWT 추출
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response); // 다음 필터로 요청 전달
+    }
+
+    // Authorization 헤더에서 토큰 추출
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // "Bearer " 부분 제거
+        }
+        return null;
+    }
+}
