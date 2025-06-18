903d537047910d511f402b7ede116ce84f1a8a25
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 11:15:24 +0900

    feat(MemberResDto): add Builder pattern and static factory method
    - Introduce Lombok's `@Builder` annotation for cleaner object construction.
    - Add static `of` method to convert `Member` entity into `MemberResDto`.

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberResDto.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberResDto.java
@@ -1,16 +1,28 @@
 package com.autoever.spring_practice.dto;
 
+import com.autoever.spring_practice.entity.Member;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
+@Builder
 public class MemberResDto {
     private String email;
     private String name;
-    private String password;
     private String image;
     private LocalDateTime regDate;
+
+    public static MemberResDto of(Member member) {
+        return MemberResDto.builder()
+                .name(member.getName())
+                .email(member.getEmail())
+                .image(member.getImage())
+                .regDate(member.getRegDate())
+                .build();
+    }
 }
