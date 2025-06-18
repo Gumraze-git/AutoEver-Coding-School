f949e32670f9448e97a1b99a0ec88152787c5a60
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 11:42:29 +0900

    feat(MemberReqDto): add conversion methods and Builder pattern
    - Include `toEntity` method for converting DTO to `Member` entity.
    - Add `toAuthenticationToken` method for generating authentication tokens.
    - Apply Lombok's `@Builder` and `@AllArgsConstructor` for streamlined object creation.

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberReqDto.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/MemberReqDto.java
@@ -1,15 +1,33 @@
 package com.autoever.spring_practice.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.autoever.spring_practice.constant.Authority;
+import com.autoever.spring_practice.entity.Member;
+import lombok.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
 @NoArgsConstructor
-
+@AllArgsConstructor
+@Builder
 public class MemberReqDto {
     private String email;
     private String password;
     private String name;
+    private String image;
+
+    public Member toEntity(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .name(name)
+                .image(image)
+                .authority(Authority.ROLE_USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthenticationToken() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
 }
