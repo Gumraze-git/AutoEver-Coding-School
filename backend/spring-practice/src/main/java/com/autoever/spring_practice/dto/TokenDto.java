8c419371c4217d5ffe8bad69ebc7186adf066329
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 10:33:19 +0900

    feat(TokenDto): add `tokenExpireIn` field

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/TokenDto.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/TokenDto.java
@@ -10,6 +10,7 @@ public class TokenDto {
     private String grantType; // 인증 방식
     private String accessToken; // 엑세스 토큰
     private String refreshToken;
+    private Long tokenExpireIn;
     private Long accessTokenExpireIn;
     private Long refreshTokenExpireIn;
 }
