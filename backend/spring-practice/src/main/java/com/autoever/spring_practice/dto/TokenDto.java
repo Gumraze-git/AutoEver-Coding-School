8f884e7b408f06796c3528ac6ef56256c3bccd94
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 10:22:01 +0900

    feat(TokenDto): extend TokenDto with grantType, accessToken, refreshToken, and expiration fields

--- backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/TokenDto.java
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/dto/TokenDto.java
@@ -1,5 +1,9 @@
 package com.autoever.spring_practice.dto;
 
 public class TokenDto {
-    private String token;
+    private String grantType; // 인증 방식
+    private String accessToken; // 엑세스 토큰
+    private String refreshToken;
+    private Long accessTokenExpireIn;
+    private Long refreshTokenExpireIn;
 }
