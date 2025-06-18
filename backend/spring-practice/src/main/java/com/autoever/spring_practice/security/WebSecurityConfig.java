8e23ff414573d33b90ccb8580b5c179a03b45593
Gumraze-git <galaxydh4110@gmail.com>
2025-06-18 11:38:03 +0900

    feat(WebSecurityConfig): add `WebSecurityConfig` for core security configuration
    - Configure HTTP security with JWT-based authentication.
    - Set stateless session management and apply `JwtSecurityConfig`.
    - Add role-based access control and endpoints permissions.

---
+++ backend/spring-practice/src/main/java/com/autoever/spring_practice/security/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.autoever.spring_practice.security;
+
+import com.autoever.spring_practice.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@Component
+public class WebSecurityConfig implements WebMvcConfigurer {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint; // 인증
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler; // 인가 실패 시 처리할 클래스
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // BCrypt 암호화 객체를 Bean으로 등록
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/board/**").permitAll()
+                .antMatchers("/api/v1/member/**").permitAll()
+                .antMatchers("/api/v1/test/**").permitAll()
+                .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+        return http.build();
+    }
+}
